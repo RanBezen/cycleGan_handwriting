e6fc531382742e4936f2acaf29ab0eb013be8740
Ran Bezen <ran.bezen@gmail.com>
2018-06-24 20:13:26 +0300

    aa

--- cycleGan.py
+++ cycleGan.py
@@ -161,68 +161,69 @@ class CycleGAN():
         return Model(img, validity)
 
     def train(self, epochs, batch_size=1, sample_interval=50):
+        with tf.device('/gpu:2'):
+            start_time = datetime.datetime.now()
 
-        start_time = datetime.datetime.now()
+            # Adversarial loss ground truths
+            valid = np.ones((batch_size,) + self.disc_patch)
+            fake = np.zeros((batch_size,) + self.disc_patch)
+            for epoch in range(epochs):
+                for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
 
-        # Adversarial loss ground truths
-        valid = np.ones((batch_size,) + self.disc_patch)
-        fake = np.zeros((batch_size,) + self.disc_patch)
+                    # ----------------------
+                    #  Train Discriminators
+                    # ----------------------
 
-        for epoch in range(epochs):
-            for batch_i, (imgs_A, imgs_B) in enumerate(self.data_loader.load_batch(batch_size)):
+                    # Translate images to opposite domain
+                    fake_B = self.g_AB.predict(imgs_A)
+                    fake_A = self.g_BA.predict(imgs_B)
 
-                # ----------------------
-                #  Train Discriminators
-                # ----------------------
+                    # Train the discriminators (original images = real / translated = Fake)
+                    dA_loss_real = self.d_A.train_on_batch(imgs_A, valid)
+                    dA_loss_fake = self.d_A.train_on_batch(fake_A, fake)
+                    dA_loss = 0.5 * np.add(dA_loss_real, dA_loss_fake)
 
-                # Translate images to opposite domain
-                fake_B = self.g_AB.predict(imgs_A)
-                fake_A = self.g_BA.predict(imgs_B)
+                    dB_loss_real = self.d_B.train_on_batch(imgs_B, valid)
+                    dB_loss_fake = self.d_B.train_on_batch(fake_B, fake)
+                    dB_loss = 0.5 * np.add(dB_loss_real, dB_loss_fake)
 
-                # Train the discriminators (original images = real / translated = Fake)
-                dA_loss_real = self.d_A.train_on_batch(imgs_A, valid)
-                dA_loss_fake = self.d_A.train_on_batch(fake_A, fake)
-                dA_loss = 0.5 * np.add(dA_loss_real, dA_loss_fake)
+                    # Total disciminator loss
+                    d_loss = 0.5 * np.add(dA_loss, dB_loss)
 
-                dB_loss_real = self.d_B.train_on_batch(imgs_B, valid)
-                dB_loss_fake = self.d_B.train_on_batch(fake_B, fake)
-                dB_loss = 0.5 * np.add(dB_loss_real, dB_loss_fake)
+                    # ------------------
+                    #  Train Generators
+                    # ------------------
 
-                # Total disciminator loss
-                d_loss = 0.5 * np.add(dA_loss, dB_loss)
+                    # Train the generators
+                    g_loss = self.combined.train_on_batch([imgs_A, imgs_B],
+                                                          [valid, valid,
+                                                           imgs_A, imgs_B,
+                                                           imgs_A, imgs_B])
+
+                    elapsed_time = datetime.datetime.now() - start_time
+
+                    # Plot the progress
+                    print(
+                        "[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f, recon: %05f, id: %05f] time: %s " \
+                        % (epoch, epochs,
+                           batch_i, self.data_loader.n_batches,
+                           d_loss[0], 100 * d_loss[1],
+                           g_loss[0],
+                           np.mean(g_loss[1:3]),
+                           np.mean(g_loss[3:5]),
+                           np.mean(g_loss[5:6]),
+                           elapsed_time))
+
+                    # If at save interval => save generated image samples
+                    if batch_i % sample_interval == 0:
+                        self.sample_images(epoch, batch_i)
+                        self.d_A.save('saved_model/cycDiscA.h5')
+                        self.d_B.save('saved_model/cycDiscB.h5')
+                        self.g_AB.save('saved_model/cycGenAB.h5')
+                        self.g_BA.save('saved_model/cycGenBA.h5')
+                        print('model saved')
 
 
-                # ------------------
-                #  Train Generators
-                # ------------------
-
-                # Train the generators
-                g_loss = self.combined.train_on_batch([imgs_A, imgs_B],
-                                                        [valid, valid,
-                                                        imgs_A, imgs_B,
-                                                        imgs_A, imgs_B])
-
-                elapsed_time = datetime.datetime.now() - start_time
-
-                # Plot the progress
-                print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f, recon: %05f, id: %05f] time: %s " \
-                                                                        % ( epoch, epochs,
-                                                                            batch_i, self.data_loader.n_batches,
-                                                                            d_loss[0], 100*d_loss[1],
-                                                                            g_loss[0],
-                                                                            np.mean(g_loss[1:3]),
-                                                                            np.mean(g_loss[3:5]),
-                                                                            np.mean(g_loss[5:6]),
-                                                                            elapsed_time))
-
-                # If at save interval => save generated image samples
-                if batch_i % sample_interval == 0:
-                    self.sample_images(epoch, batch_i)
-                    self.d_A.save('saved_model/cycDiscA.h5')
-                    self.d_B.save('saved_model/cycDiscB.h5')
-                    self.g_AB.save('saved_model/cycGenAB.h5')
-                    self.g_BA.save('saved_model/cycGenBA.h5')
-                    print('model saved')
 
     def sample_images(self, epoch, batch_i):
         #os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
