4861f501a8bd72a47805338f47fd726b938a15a9
Ran Bezen <ran.bezen@gmail.com>
2018-06-26 21:59:02 +0300

    Update cycleGan.py

--- cycleGan.py
+++ cycleGan.py
@@ -22,7 +22,6 @@ class CycleGAN():
     def __init__(self):
         # Input shape
 	
-	self.gpu_num='/gpu:1'
         self.img_rows = 48
         self.img_cols = 512
         self.channels = 1
@@ -104,7 +103,7 @@ class CycleGAN():
 
     def build_generator(self):
         """U-Net Generator"""
-        with tf.device(self.gpu_num):
+        with tf.device('/gpu:1'):
             def conv2d(layer_input, filters, f_size=4):
                 """Layers used during downsampling"""
                 d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
@@ -143,7 +142,7 @@ class CycleGAN():
             return Model(d0, output_img)
 
     def build_discriminator(self):
-        with tf.device(self.gpu_num):
+        with tf.device('/gpu:1'):
             def d_layer(layer_input, filters, f_size=4, normalization=True):
                 """Discriminator layer"""
                 d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
@@ -166,7 +165,7 @@ class CycleGAN():
 
 
     def train(self, epochs, batch_size=1, sample_interval=50):
-        with tf.device(self.gpu_num):
+        with tf.device('/gpu:1'):
             start_time = datetime.datetime.now()
 
             # Adversarial loss ground truths
